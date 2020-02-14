7a1b2514bb5833d32c6bf1d63bb30e0790341f6b
Mbuso-Biyela <60347808+Mbuso-Biyela@users.noreply.github.com>
2020-02-14 15:51:06 +0200

    ADD TO CODE TO FUNCTIONS

--- Functions/Function_5.py
+++ Functions/Function_5.py
@@ -1,1 +1,13 @@
-# Add Code Here
+### START FUNCTION
+def number_of_tweets_per_day(df):
+
+    # Isolate Date from DateTime Format
+    df["Date"] = date_parser(df["Date"].to_list())
+
+    #Set all Tweets to 1 so they can be counted
+    df["Tweets"] = 1
+
+    # Group By Date and Sum Tweets
+    return df.groupby(["Date"]).sum()
+
+### END FUNCTION
