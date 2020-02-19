1e0d8b16690ff484f364f841d1acc03cfd8f263b
Amukelani-Ngobeni <60642268+Amukelani-Ngobeni@users.noreply.github.com>
2020-02-19 15:36:39 +0200

    Doctstring 5 Update

--- Functions/Function_5.py
+++ Functions/Function_5.py
@@ -4,19 +4,44 @@ def number_of_tweets_per_day(df):
 
     The function calculates the number of tweets that were posted per day.
 
-    Arguments:
+    Arguments: The variable 'df' is the pandas input.
 
-    The index of the new dataframe is 'Date' in the format:
-
-        'yyyy-mm-dd',
-
+    Returns: df
+    The index of the new dataframe is 'Date' in the format:'yyyy-mm-dd',
     and the column of the new dataframe is 'Tweets',
-
     corresponding to the date and number of tweets, respectively.
-
     The functiion returns the number of tweets per day.
 
+    Example:
+    Prerequites:
+    >>> twitter_url = 'https://raw.githubusercontent.com/Explore-AI/Public-Data/master/Data/twitter_nov_2019.csv'
+    >>> twitter_df = pd.read_csv(twitter_url)
 
+    Inputs:
+    >>>twitter_df.copy().head()
+
+        Tweets	                                            Date
+    0	@BongaDlulane Please send an email to mediades...	2019-11-29 12:50:54
+    1	@saucy_mamiie Pls log a call on 0860037566	        2019-11-29 12:46:53
+    2	@BongaDlulane Query escalated to media desk.	     2019-11-29 12:46:10
+    3	Before leaving the office this afternoon, head...	2019-11-29 12:33:36
+    4	#ESKOMFREESTATE #MEDIASTATEMENT : ESKOM SUSPEN...	2019-11-29 12:17:43
+
+    >>>number_of_tweets_per_day(twitter_df.copy())
+    Output
+                Tweets
+    Date
+    2019-11-20	18
+    2019-11-21	11
+    2019-11-22	25
+    2019-11-23	19
+    2019-11-24	14
+    2019-11-25	20
+    2019-11-26	32
+    2019-11-27	13
+    2019-11-28	32
+    2019-11-29	16
+    
     """
 
     # Isolate Date from DateTime Format
