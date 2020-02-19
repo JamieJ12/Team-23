aceff7dc7097c37c0f7c5936958e78aeb9e221fc
MPUMELELONDLOVU41 <60341189+MPUMELELONDLOVU41@users.noreply.github.com>
2020-02-19 14:37:59 +0200

    setup edit done

--- setup.py
+++ setup.py
@@ -5,10 +5,10 @@ setup(
     version='0.1',
     packages=find_packages(exclude=['tests*']),
     license='MIT',
-    description='EDSA example python package',
+    description='Functions of Eskom data metrics.',
     long_description=open('README.md').read(),
     install_requires=['numpy'],
     url='https://github.com/JamieJ12/Team-23',
-    author='Team LeBron 23',
+    author='Team LeBron 23(Jamie Japhta, Mpumelelo Ndlovu, Precious Sekgathume, Amukelani Ngobeni, Mbuso Biyela)',
     author_email='teamlebron23@gmail.com'
 )
